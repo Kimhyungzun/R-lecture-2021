5aedcccb0b511f1f1ed6cbfde2193e78eb34fc53
Kimhyungzun <procss2696@naver.com>
2021-04-28 11:36:31 +0900

    chapter 6

--- chapter6/ch06-1.R
+++ chapter6/ch06-1.R
@@ -21,3 +21,89 @@ for (i in 1:4) {
 # 요약
 summary(anscombe)
 
+# 그래프 그리기
+library(ggplot2)
+ggplot(anscombe) +
+      geom_point(aes(x1,y1),color='darkorange',size=3) +
+      scale_x_continuous(breaks=seq(2,20,2)) +
+      scale_y_continuous(breaks = seq(2,14,2)) +
+      xlim(2,20) +
+      ylim(2,14) +
+      geom_abline(slope=0.5, intercept = 3,
+                  color='cornflowerblue', size=1) +
+      labs(title='Dataset ')      
+      
+p1 <- ggplot(anscombe) +
+      geom_point(aes(x1,y1),color='darkorange',size=3) +
+      scale_x_continuous(breaks = seq(2,20,2)) +
+      scale_y_continuous(breaks = seq(2,14,2)) +
+      xlim(2,20) +
+      ylim(2,14) +
+      geom_abline(intercept = 3,slope = 0.5,color='cornflowerblue',size=1) +
+      labs(title='Dataset 1') 
+
+p2 <- ggplot(anscombe) +
+      geom_point(aes(x2,y2),color='darkorange',size=3) +
+      scale_x_continuous(breaks=seq(2,20,2)) +
+      scale_y_continuous(breaks = seq(2,14,2)) +
+      xlim(2,20) +
+      ylim(2,14) +
+      geom_abline(intercept = 3,slope = 0.5,color='cornflowerblue',size=1) +
+      labs(title='Dataset 2') 
+
+p3 <- ggplot(anscombe) +
+      geom_point(aes(x3,y3),color='darkorange',size=3) +
+      scale_x_continuous(breaks=seq(2,20,2)) +
+      scale_y_continuous(breaks = seq(2,14,2)) +
+      xlim(2,20) +
+      ylim(2,14) +
+      geom_abline(intercept = 3,slope = 0.5,color='cornflowerblue',size=1) +
+      labs(title='Dataset 3') 
+
+p4 <- ggplot(anscombe) +
+      geom_point(aes(x4,y4),color='darkorange',size=3) +
+      scale_x_continuous(breaks=seq(2,20,2)) +
+      scale_y_continuous(breaks = seq(2,14,2)) +
+      xlim(2,20) +
+      ylim(2,14) +
+      geom_abline(intercept = 3,slope = 0.5,color='cornflowerblue',size=1) +
+      labs(title='Dataset 4')
+
+library(gridExtra)      
+grid.arrange(p1,p2,p3,p4, ncol=2, top="Anscombe's Quartet")
+
+###################################################################3
+
+figures <- list()
+figures <- append(figures,p1)
+figures <- append(figures,p2)
+figures <- append(figures,p3)
+figures <- append(figures,p4)
+figures
+
+# Source Refactoring
+x <- ggplot(anscombe) +
+     geom_point(aes(x4,y4),color='darkorange',size=3)
+m1 <- x
+assign(paste('m', 4, sep='.'), x)
+grid.arrange(p1,p2,p3,m.4, ncol=2, top="Anscombe's Quartet")
+
+library(dplyr)
+      x <- ggplot(anscombe) +
+           geom_point(aes(anscombe[,i],anscombe[,i+4]),color='darkorange',size=3) +
+           scale_x_continuous(breaks=seq(2,20,2)) +
+           scale_y_continuous(breaks = seq(2,14,2)) +
+           xlim(2,20) +
+           ylim(2,14) +
+           geom_abline(intercept = 3,slope = 0.5,color='cornflowerblue',size=1) +
+           labs(title=paste0('Dataset', i), x=paste0('x',i), y=paste0('y', i)) +
+           assign(paste('m', i, sep='.'), x)
+     
+     
+
+grid.arrange(m.1,m.2,m.3,m.4, ncol=2, top="Anscombe's Quartet")
+
+
+
+
+
